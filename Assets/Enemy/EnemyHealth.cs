766be28bf8eae6cd48833008cc37111de653bedc
TCLowe <errantknight7@gmail.com>
2021-06-25 11:23:02 -0500

    Added hit detection on enemies, and destroy on death

---
+++ Assets/Enemy/EnemyHealth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] int maxHP = 5;
+    int currentHP = 0;    
+
+    void Start()
+    {
+        currentHP = maxHP;
+    }
+
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+
+    void ProcessHit()
+    {
+        Debug.Log("Enemy Hit");
+        currentHP--;
+        if (currentHP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
